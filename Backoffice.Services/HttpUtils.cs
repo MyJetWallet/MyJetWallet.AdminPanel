38a66b41c0ae38b6ba9a076fe7e9586c6212085f
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-25 19:02:55 +0200

    remove debug log

--- Backoffice.Services/HttpUtils.cs
+++ Backoffice.Services/HttpUtils.cs
@@ -20,7 +20,6 @@ namespace Backoffice.Services
             }
             else
             {
-                Console.WriteLine($"x-ssl-user: {sslUser.ToString()}");
                 sslCn = sslUser.ToString().Split("=").Last();
             }
             var targetUser = await BoUsersService.GetBoUserById(sslCn);
