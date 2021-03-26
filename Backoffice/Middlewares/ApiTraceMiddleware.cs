162ab457a47ffb3e174e88eced7d94e8b9229650
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-26 10:56:06 +0200

    update

--- Backoffice/Middlewares/ApiTraceMiddleware.cs
+++ Backoffice/Middlewares/ApiTraceMiddleware.cs
@@ -18,15 +18,26 @@ namespace Backoffice.Middlewares
 
         public async Task InvokeAsync(HttpContext context)
         {
-            var path = context.Request.Path;
+            var path = context.Request.Path.ToString();
             var method = context.Request.Method;
             var schema = context.Request.Scheme;
-            
-            Console.WriteLine($"Http call {schema} {method} {path}");
-            
-            await _next.Invoke(context);
-            
-            Console.WriteLine($"Http call DONE {schema} {method} {path}");
+
+            if (!path.Contains("isalive") && !path.Contains("metrics"))
+            {
+                var user = context.Request.Headers.TryGetValue("", out var sslCn)
+                    ? sslCn.ToString()
+                    : "no user";
+                        
+                Console.WriteLine($"Http call {schema} {method} {path} | {user}");
+
+                await _next.Invoke(context);
+
+                Console.WriteLine($"Http call DONE {schema} {method} {path}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
         }
     }
 }
