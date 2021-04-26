f55d85c2757171ba75a40e247a3e30608e178f02
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-04-26 10:58:22 +0300

    update

--- Backoffice/SettingsModels.cs
+++ Backoffice/SettingsModels.cs
@@ -4,51 +4,12 @@ namespace Backoffice
 {
     public class SettingsModel
     {
-        [YamlProperty("AdminPanel.AccountsGrpcHostPort")]
-        public string AccountsGrpcHostPort { get; set; }
-
-        [YamlProperty("AdminPanel.TransactionsGrpcHostPort")]
-        public string TransactionsGrpcHostPort { get; set; }
-
-        [YamlProperty("AdminPanel.PersonalDataGrpcHostPort")]
-        public string PersonalDataGrpcHostPort { get; set; }
-
-        [YamlProperty("AdminPanel.ActiveDealsGrpcService")]
-        public string ActiveDealsGrpcService { get; set; }
-        
-        [YamlProperty("AdminPanel.DepositManagerGrpcService")]
-        public string DepositManagerGrpcService { get; set; }
-
-        [YamlProperty("AdminPanel.LogsGrpcUrl")] 
-        public string LogsGrpcUrl { get; set; }
-
-        [YamlProperty("AdminPanel.KycGrpcUrl")] 
-        public string KycGrpcUrl { get; set; }
-
-        [YamlProperty("AdminPanel.StatusesGrpcUrl")] 
-        public string StatusesGrpcUrl { get; set; }
-
-        [YamlProperty("AdminPanel.AuthGrpcServiceUrl")]
-        public string AuthGrpcServiceUrl { get; set; }
-
-        [YamlProperty("AdminPanel.PhonePoolServiceUrl")]
-        public string PhonePoolServiceUrl { get; set; }
-
         [YamlProperty("AdminPanel.TableStorageConnectionString")]
         public string TableStorageConnectionString { get; set; }
 
         [YamlProperty("AdminPanel.TokenKey")] 
         public string TokenKey { get; set; }
         
-        [YamlProperty("AdminPanel.MonfexApi")] 
-        public string MonfexApi { get; set; }
-
-        [YamlProperty("AdminPanel.AlianceApi")] 
-        public string AlianceApi { get; set; }
-
-        [YamlProperty("AdminPanel.HpApi")] 
-        public string HpApi { get; set; }
-
         [YamlProperty("AdminPanel.SeqServiceUrl")]
         public string SeqServiceUrl { get; set; }
 
