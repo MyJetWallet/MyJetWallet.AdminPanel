a5fadcba0611ea095560228e679f2d539a60d677
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-08-13 12:29:22 +0300

    fix

--- Backoffice/Mocks/MyCrmPaymentsReportGrpcServiceMoq.cs
+++ Backoffice/Mocks/MyCrmPaymentsReportGrpcServiceMoq.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyCRM.Logs.GrpcContracts.Models;
 using MyCrm.PaymentReport.GrpcContracts;
 using MyCrm.PaymentReport.GrpcContracts.Contracts;
 using MyCrm.PaymentReport.GrpcContracts.Models;
@@ -8,6 +10,16 @@ namespace Backoffice.Mocks
     public class MyCrmPaymentsReportGrpcServiceMoq: IMyCrmPaymentsReportGrpcService
     {
         public IAsyncEnumerable<PaymentGrpcModel> GetPaymentsAsync(GetPaymentsGrpcRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<PaginatedPaymentsGrpcModel> GetPaginatedPaymentsAsync(GetPaginatedPaymentsGrpcRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<(string value, string displayValue)>> GetPossibleValuesAsync(GetPossibleValuesGrpcRequest request)
         {
             throw new System.NotImplementedException();
         }
